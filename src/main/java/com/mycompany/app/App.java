51cb75af6d57d138e9bb6aa422056bf97a399862
pravesh-77 <156897162+pravesh-77@users.noreply.github.com>
2024-02-03 15:53:26 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,14 +2,15 @@ package com.mycompany.app;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Introduce an error - Division by zero to cause a compilation error
+        int result = 10 / 0;
     }
 }
+
 
 
