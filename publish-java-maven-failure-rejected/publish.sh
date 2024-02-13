db8e3c23a57cfb158d24eeeec597076323ddc2bd
pravesh-77 <https://github.com/pravesh-77/publish-java-package-with-maven>
2024-02-13 15:01:14 +0530

    deleted a directory

--- publish-java-maven-failure-rejected/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Maven deploy
-mvn deploy -DskipTests || { 
-	    echo "Maven deployment failed. Moving artifacts to Rejected directory."
-    mv target/*.jar Rejected/
-        exit 1
-}
-
